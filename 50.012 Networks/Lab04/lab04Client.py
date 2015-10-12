dbe9b6bf095c2bf16a444f8fd2aa56a31cbc9ca8
dabinlee708 <dabinlee708@desktop>
2015-10-12 17:12:47 +0800

    commit

--- 50.012 Networks/Lab04/lab04Client.py
+++ 50.012 Networks/Lab04/lab04Client.py
@@ -54,33 +54,33 @@ for washDry in range(0,2):
         register(base2,base3,x)
         checkAvail(base2,base3)
         
-while True:
-    try:
-        nl=list(input("Type 'x' to escape\nDo you want to change status of any machine? \nIf yes, please type [MachineType,Block,ID,State]\n For drier, type 0 \n for washer type 1\n Example:\n 0,55,2,'Available' will change the state of Block 55 Drier 0 to 'Available'\n"))
-        a=nl[0]
-    except:
-        print "Invalid Command"
-    if nl=='x':
-        break;
-    else:
-        if nl[0]==0:
-            base2='drier?'
-        elif nl[0]==1:
-            base2='washer?'
-        else:
-            print "Invalid Command"
-        if nl[1]==55:
-            base3='blk=55'
-        elif nl[1]==57:
-            base3='blk=57'
-        elif nl[1]==59:
-            base3='blk=59'
-        else:
-            print "Invalid Command"
-        base4='&id='+str(nl[2])
-        print base+base2+base3+base4+"&state="+nl[3]
-        r = requests.put(base+base2+base3+base4+"&state="+nl[3])
-        if r.status_code=='500':
-            print "Invalid machine"
-        else:
-            print r.text
+# while True:
+#     try:
+#         nl=list(input("Type 'x' to escape\nDo you want to change status of any machine? \nIf yes, please type [MachineType,Block,ID,State]\n For drier, type 0 \n for washer type 1\n Example:\n 0,55,2,'Available' will change the state of Block 55 Drier 0 to 'Available'\n"))
+#         a=nl[0]
+#     except:
+#         print "Invalid Command"
+#     if nl=='x':
+#         break;
+#     else:
+#         if nl[0]==0:
+#             base2='drier?'
+#         elif nl[0]==1:
+#             base2='washer?'
+#         else:
+#             print "Invalid Command"
+#         if nl[1]==55:
+#             base3='blk=55'
+#         elif nl[1]==57:
+#             base3='blk=57'
+#         elif nl[1]==59:
+#             base3='blk=59'
+#         else:
+#             print "Invalid Command"
+#         base4='&id='+str(nl[2])
+#         print base+base2+base3+base4+"&state="+nl[3]
+#         r = requests.put(base+base2+base3+base4+"&state="+nl[3])
+#         if r.status_code=='500':
+#             print "Invalid machine"
+#         else:
+#             print r.text
