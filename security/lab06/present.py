fb7683d3e71c38ef7442a91d2439b187da0c1131
dabin.lee@outlook.com <Chwb5278!>
2016-03-17 22:53:43 +0800

    lab6 updated Step 1 for key generation done

--- security/lab06/present.py
+++ security/lab06/present.py
@@ -23,8 +23,20 @@ ror = lambda val, r_bits, max_bits: \
     ((val & (2**max_bits-1)) >> r_bits%max_bits) | \
     (val << (max_bits-(r_bits%max_bits)) & (2**max_bits-1))
 
+
+# keys={}
+def binary(num, pre='0b', length=8, spacer=0):
+    return '{0}{{:{1}>{2}}}'.format(pre, spacer, length).format(bin(num)[2:])
+
 def genRoundKeys(key):
-    pass
+    print "Original Key: ",binary(key,'ob',80,0)
+    step1=rol(key,61,80)
+    print "Step 1   key: ",binary(step1,'ob',80,0)
+
+    # pass
+    # keylength=len(key)
+    # for i in range(len(key)):
+    #     keys[keylength-i]=key[i]
 
 def addRoundKey(state,Ki):
     pass
@@ -44,32 +56,36 @@ def present(plain, key):
 def present_inv(plain, key):
     pass
 
+key=0xabcdeabcdeabcdeabcde
+genRoundKeys(key)
 
 
-if __name__=="__main__":
-    plain1=0x0000000000000000
-    key1=0x00000000000000000000
-    cipher1= present(plain1,key1)
-    plain11 = present_inv(cipher1,key1)
-    print format(cipher1,'x')
-    print format(plain11,'x')
-    plain2=0x0000000000000000
-    key2=0xFFFFFFFFFFFFFFFFFFFF
-    cipher2= present(plain2,key2)
-    plain22 = present_inv(cipher2,key2)
-    print format(cipher2,'x')
-    print format(plain22,'x')
-    plain3=0xFFFFFFFFFFFFFFFF
-    key3=0x00000000000000000000
-    cipher3= present(plain3,key3)
-    plain33 = present_inv(cipher3,key3)
-    print format(cipher3,'x')
-    print format(plain33,'x')
-    plain4=0xFFFFFFFFFFFFFFFF
-    key4=0xFFFFFFFFFFFFFFFFFFFF
-    cipher4= present(plain4,key4)
-    plain44 = present_inv(cipher4,key4)
-    print format(cipher4,'x')
-    print format(plain44,'x')
+# if __name__=="__main__":
+#     rval=rol(0b1001,1,4)
+#     print "result of rol is %x"%rval
+#     plain1=0x0000000000000000
+#     key1=0x00000000000000000000
+#     cipher1= present(plain1,key1)
+#     plain11 = present_inv(cipher1,key1)
+#     print format(cipher1,'x')
+#     print format(plain11,'x')
+#     plain2=0x0000000000000000
+#     key2=0xFFFFFFFFFFFFFFFFFFFF
+#     cipher2= present(plain2,key2)
+#     plain22 = present_inv(cipher2,key2)
+#     print format(cipher2,'x')
+#     print format(plain22,'x')
+#     plain3=0xFFFFFFFFFFFFFFFF
+#     key3=0x00000000000000000000
+#     cipher3= present(plain3,key3)
+#     plain33 = present_inv(cipher3,key3)
+#     print format(cipher3,'x')
+#     print format(plain33,'x')
+#     plain4=0xFFFFFFFFFFFFFFFF
+#     key4=0xFFFFFFFFFFFFFFFFFFFF
+#     cipher4= present(plain4,key4)
+#     plain44 = present_inv(cipher4,key4)
+#     print format(cipher4,'x')
+#     print format(plain44,'x')
 
 
